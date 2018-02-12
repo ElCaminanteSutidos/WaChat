6fb8118757742970bd9288e482ab87eaba7bfa10
PC2 <PC2@PC2>
2018-02-12 17:54:12 -0600

    Server and Client Run Configurations.

--- src/com/El/Caminante/WaChat/Main/Main.java
+++ src/com/El/Caminante/WaChat/Main/Main.java
@@ -1,9 +1,22 @@
 package com.El.Caminante.WaChat.Main;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		System.out.println("WaChat!");
+		boolean isServer = false;
+		if (args.length > 0) {
+			isServer = args[0].toLowerCase().equals("-server");
+		}
+		new Main(isServer);
+	}
+
+	public Main(boolean isServer) {
+		if (isServer) {
+			System.out.println("WaChat Server!");
+		} else {
+			System.out.println("WaChat Client!");
+		}
+
 	}
 
 }
